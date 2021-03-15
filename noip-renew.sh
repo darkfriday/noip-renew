79b8417e7a18ef8347dc4cae46d2dca4766d8669
darkfriday <11607270+darkfriday@users.noreply.github.com>
2021-03-15 20:30:34 +0800

    change log format

--- noip-renew.sh
+++ noip-renew.sh
@@ -10,5 +10,5 @@ if [ -z "$LOGDIR" ]; then
     $PROGDIR/noip-renew.py "$USERNAME" "$PASSWORD" 2
 else
     cd $LOGDIR
-    $PROGDIR/noip-renew.py "$USERNAME" "$PASSWORD" 0 >> $USERNAME.log
+    $PROGDIR/noip-renew.py "$USERNAME" "$PASSWORD" >>$USERNAME.log 2>&1
 fi
