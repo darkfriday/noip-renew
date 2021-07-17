76d4058f95522b95b2c70c3107b62bbcfac9dab0
darkfriday <11607270+darkfriday@users.noreply.github.com>
2021-07-17 12:06:00 +0800

    modify crontab time @3:30am

--- noip-renew-skd.sh
+++ noip-renew-skd.sh
@@ -4,8 +4,8 @@ SUDO=sudo
 LOGDIR=/var/log/noip-renew/$USER
 INSTDIR=/usr/local/bin
 INSTEXE=$INSTDIR/noip-renew-$USER.sh
-CRONJOB="30 0 * * * $INSTEXE $LOGDIR"
-NEWCJOB="30 0 $1 $2 * $INSTEXE $LOGDIR"
+CRONJOB="30 3 * * * $INSTEXE $LOGDIR"
+NEWCJOB="30 3 $1 $2 * $INSTEXE $LOGDIR"
 crontab -u $USER -l | grep -v '/noip-renew*'  |  crontab -u $USER -
 if [[ $3 = "True" ]]; then
     (crontab -u $USER -l; echo "$NEWCJOB") |  crontab -u $USER -
